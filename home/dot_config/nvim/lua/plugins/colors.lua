3b4c1969c3bcfa689057b97ed2de7fff6cc6d771
Koray Aydemir <korayaydemir99@gmail.com>
2025-04-12 19:40:48 +0300

    refactor aformat

--- home/dot_config/nvim/lua/plugins/colors.lua
+++ home/dot_config/nvim/lua/plugins/colors.lua
@@ -1,15 +1,15 @@
 local function config()
-    vim.cmd.colorscheme("gruvbox")
+	vim.cmd.colorscheme("gruvbox")
 
-    --vim.cmd([[hi Comment guifg='red' ]])
-    vim.cmd([[hi Comment guifg='#fddddd' ]])
+	--vim.cmd([[hi Comment guifg='red' ]])
+	vim.cmd([[hi Comment guifg='#fddddd' ]])
 
-    vim.g.gitblame_highlight_group = "GitBlame"
-    vim.cmd([[hi GitBlame guifg='#aaaaaa' guibg=none]])
+	vim.g.gitblame_highlight_group = "GitBlame"
+	vim.cmd([[hi GitBlame guifg='#aaaaaa' guibg=none]])
 
-    vim.cmd([[hi StatusLine guifg='#252525' guibg='#dcdcdc']])
+	vim.cmd([[hi StatusLine guifg='#252525' guibg='#dcdcdc']])
 
-    vim.cmd([[hi TelescopeResultsComment guifg='#fbf1c7' guibg='#282828']])
+	vim.cmd([[hi TelescopeResultsComment guifg='#fbf1c7' guibg='#282828']])
 end
 
 return { "morhetz/gruvbox", priority = 100, config = config }
