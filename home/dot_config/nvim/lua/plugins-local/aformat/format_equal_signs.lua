cca41f2bc72bb4784a8a7883199dd5d97a15fb29
Koray Aydemir <korayaydemir99@gmail.com>
2025-05-28 21:59:48 +0300

    fix format equal signs for one line consecutive jsx elements
    its to prevent formatting like this:
    <div className     = "someclass">
        <div className = "someotherclass"></div>
    </div>

--- home/dot_config/nvim/lua/plugins-local/aformat/format_equal_signs.lua
+++ home/dot_config/nvim/lua/plugins-local/aformat/format_equal_signs.lua
@@ -1,9 +1,24 @@
 M = {}
 
+local function is_consecutive(arr)
+  if #arr <= 1 then
+    return false
+  end
+
+  table.sort(arr)
+  for i = 2, #arr do
+    if arr[i] ~= arr[i-1] + 1 then
+      return false
+    end
+  end
+  return true
+end
+
+local consecutive_line_blocks_of_jsx_element_attributes = {}
 ---@return table
 local function find_equal_sign_lines()
 	local ts = vim.treesitter
-    -- [intentional-no-pcall]
+	-- [intentional-no-pcall]
 	local parser = ts.get_parser()
 
 	local tree = parser:parse()[1]
@@ -13,9 +28,10 @@ local function find_equal_sign_lines()
 		"javascript",
 		[[ 
             (lexical_declaration (variable_declarator) @equals)
-            (assignment_expression) @equals
+           (assignment_expression) @equals
 
-            (jsx_attribute (property_identifier) @jsx_equals)
+           (jsx_self_closing_element) @jsx_element
+           (jsx_opening_element)      @jsx_element
         ]]
 	)
 
@@ -24,19 +40,37 @@ local function find_equal_sign_lines()
 		local capture_name = query.captures[id]
 
 		if capture_name == "equals" then
-			local second_child_type = node:named_child(1):type()
-			if second_child_type ~= "arrow_function" then
+			local second_child = node:named_child(1)
+
+            -- it matched a variable declaration without equals sign.
+            -- e.g. `let myVariable;`
+			if second_child == nil then goto continue end
+
+			if second_child:type() ~= "arrow_function" then
 				local start_row, _, _, _ = node:range() -- 0 indexed
 				local line = start_row + 1
 				table.insert(results, line)
 			end
 		end
 
-		if capture_name == "jsx_equals" then
-			local start_row, _, _, _ = node:range() -- 0 indexed
-            local line = start_row + 1
-            table.insert(results, line)
+		if capture_name == "jsx_element" then
+            local line_group = {}
+            for child, name in node:iter_children() do
+                if name == 'attribute' then
+                    local start_row, _,_,_ = child:range()
+                    local line = start_row + 1
+                    table.insert(line_group, line)
+                end
+            end
+            if is_consecutive(line_group) then
+                for k,v in ipairs(line_group) do
+                    print(k.." = "..v)
+                end
+                vim.list_extend(consecutive_line_blocks_of_jsx_element_attributes, line_group)
+            end
 		end
+
+        ::continue::
 	end
 	return results
 end
@@ -78,7 +112,6 @@ end
 M.format_equal_signs = function()
 	local lines = find_equal_sign_lines()
 	local blocks = get_consecutive_line_blocks(lines)
-
 	for _, block in ipairs(blocks) do
 		local start_line = block.start_line - 1
 		local end_line = block.end_line - 1 + 1 -- -1 for 0 indexing, +1 for nvim_buf.. functions having exclusive end line
@@ -89,6 +122,19 @@ M.format_equal_signs = function()
 		-- Replace original lines with aligned ones
 		vim.api.nvim_buf_set_lines(0, start_line, end_line, false, aligned)
 	end
+
+	local blocks2 = get_consecutive_line_blocks(consecutive_line_blocks_of_jsx_element_attributes)
+	for _, block in ipairs(blocks2) do
+		local start_line = block.start_line - 1
+		local end_line = block.end_line - 1 + 1 -- -1 for 0 indexing, +1 for nvim_buf.. functions having exclusive end line
+		local block_lines = vim.api.nvim_buf_get_lines(0, start_line, end_line, false)
+
+		local aligned = mini_align_equal_signs(block_lines)
+
+		-- Replace original lines with aligned ones
+		vim.api.nvim_buf_set_lines(0, start_line, end_line, false, aligned)
+	end
+
 end
 
 return M
